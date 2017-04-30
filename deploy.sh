8d8e3ffe6c4317171a4757912582c868f3c3b86c
Gurvinder Singh <gurvinder.singh@uninett.no>
2017-04-30 16:05:24 +0200

    fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TAG=$1
 
-docker build -t quay.io/$TRAVIS_REPO_SLUG:$TAG .;
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io;
-docker push quay.io/$TRAVIS_REPO_SLUG:$TAG;
+echo "Building image with Tag: $TAG"
+docker build -t quay.io/$TRAVIS_REPO_SLUG:$TAG .
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
+docker push quay.io/$TRAVIS_REPO_SLUG:$TAG
